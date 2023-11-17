372eedc59d09cf1a587f8539660fc49a4c0004b3
Galang Arsandy <galangarsandy@gmail.com>
2023-11-17 20:32:30 +0700

    Init Stucture Folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-red-900 m-10">Welcome to Mangan</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-100': '#de283b',
+        'primary-200': '#ff6366',
+        'primary-300': '#ffccc4',
+        'accent-100': '#25b1bf',
+        'accent-200': '#005461',
+        'text-100': '#1a1a1a',
+        'texr-200': '#404040',
+        'bg-100': '#ffffff',
+        'bg-200': '#f5f5f5',
+        'bg-300': '#cccccc',
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
