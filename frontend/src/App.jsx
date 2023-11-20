f922c5c2f108075dd3fa4723515e4d5aea781ead
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-11-20 12:14:02 +0700

    make a router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import Dashboard from "./seller/components/dashboard"
+import { Route,Routes } from "react-router-dom"
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-red-900 m-10">Welcome to Mangan</h1>
-    </>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  )
+}
+
+function Home(){
+  return(
+    <h1 className="font-bold text-2xl text-red-900 m-10">Welcome to Mangan</h1>
   )
 }
 
