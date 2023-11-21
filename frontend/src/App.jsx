17d778c4bbb601b7769bd278ea2a980b4aa4e3bc
Galang Arsandy <galangarsandy@gmail.com>
2023-11-22 00:45:29 +0700

    Fixing Browser Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import Dashboard from "./seller/components/dashboard"
-import { Route, Routes } from "react-router-dom"
-import LandingPage from "./user/pages/LandingPage"
+import User from "./user/User"
+import Seller from "./seller/Seller"
 
 function App() {
   return (
     <div className="bg-[#D8DEDF]">
-      <Routes>
-        {/* User Route */}
-        <Route path="/" element={<LandingPage />} />
-
-        {/* Seller Route */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <User />
+      <Seller />
     </div>
   )
 }
