b045e6a28aaf1f1336e9df9074b0bc6f698f8959
Galang Arsandy <galangarsandy@gmail.com>
2023-11-22 02:41:07 +0700

    Add Footer Component

--- frontend/src/user/components/Button.jsx
+++ frontend/src/user/components/Button.jsx
@@ -1,18 +1,9 @@
 function Button(props) {
-    function buttonType() {
-        switch (props.tipe) {
-            case 'primary':
-                return 'bg-primary-100'
-            case 'secondary':
-                return 'bg-primary-200'
-            default:
-                return 'bg-primary-100'
-        }
-    }
+
     return (
         <>
             <button
-                className={buttonType() + 'px-4 py-1 rounded text-white font-normal hover:bg-opacity-75 active:bg-primary-100'}
+                className='px-4 py-1 bg-primary-100 rounded text-white font-medium hover:bg-opacity-75 active:bg-primary-100'
             >{props.name}</button>
         </>
     )
--- frontend/src/user/components/Navbar.jsx
+++ frontend/src/user/components/Navbar.jsx
@@ -3,19 +3,31 @@ import LogoMangan from '/logo-mangan.svg'
 
 function Navbar() {
     return (
-        <div className='flex justify-between items-center px-16 h-20 bg-white '>
-            <div className='flex gap-10 h-full items-center'>
-                <img src={LogoMangan} alt="" width={100} />
-                <div className='flex gap-2'>
-                    <span>Home</span>
-                    <span>Home</span>
-                    <span>Home</span>
-                    <span>Home</span>
+        <div className='flex justify-between items-center px-20 h-20 bg-white '>
+            <div className='flex gap-28 h-full items-center'>
+                <img src={LogoMangan} alt="" width={120} />
+                <div className='flex gap-8 '>
+                    <span>
+                        <a href=""
+                            className='font-bold text-primary-100 hover:text-opacity-75 active:text-primary-100'>Home</a>
+                    </span>
+                    <span>
+                        <a href=""
+                            className='font-bold text-primary-100 hover:text-opacity-75 active:text-primary-100'>Tentang Kami</a>
+                    </span>
+                    <span>
+                        <a href=""
+                            className='font-bold text-primary-100 hover:text-opacity-75 active:text-primary-100'>Katering</a>
+                    </span>
+                    <span>
+                        <a href=""
+                            className='font-bold text-primary-100 hover:text-opacity-75 active:text-primary-100'>Bergabung</a>
+                    </span>
                 </div>
             </div>
-            <div className='flex gap-2'>
-                <LocalButton name="Login" tipe='primary' />
-
+            <div className='flex gap-4 items-center'>
+                <a href="" className='font-semibold text-primary-100 hover:text-opacity-75 active:text-primary-100'>Daftar</a>
+                <LocalButton name="Masuk" />
             </div>
         </div>
     )
