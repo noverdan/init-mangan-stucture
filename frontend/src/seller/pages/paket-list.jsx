b04bea9ec26fba86a8e14aff7303470968a09234
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-12-08 22:42:32 +0700

    add responsive

--- frontend/src/seller/pages/paket-list.jsx
+++ frontend/src/seller/pages/paket-list.jsx
@@ -22,30 +22,31 @@ function PaketList() {
 
 
     return (
-        <div className={`flex items-start  h-auto overflow-hidden transition-all ease-in-out duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-1'}`}>
-            <div className="flex">
+        <div className={`flex items-start bg-bg-300 h-screen overflow-auto transition-all ease-in-out duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-1'}`}>
+            <div className="flex  h-full">
                 {isOpen && <Sidebar />}
                 <button className='z-20 w-9 h-9  rounded-full transition-colors ' onClick={toggleSidebar}>
                     <Hamburger size={20} easing="ease-in" color='black' toggled={isOpen} toggle={setIsOpen} />
                 </button>
             </div>
-            <div className="flex h-[100%]  flex-wrap">
+            <div className="flex h-full overflow-auto  flex-wrap">
                 {sortedProducts.map((paket) => (
-                    <div className="m-9 w-[200px]  h-[277px] px-[26px] py-[18px] bg-slate-50 rounded-[15px] border border-sky-900 ">
+                    <div className="m-9 w-[200px] flex flex-col gap-5  h-[277px] px-[26px] py-[18px] bg-white rounded-[15px] border border-sky-900 ">
                         <img className="w-[148px] h-[137px] rounded-[10px] bg-bg-300" src={paket.image} alt="" />
-                        <div className="mt-4">
-                            <h1 className="text-accent-200 font-semibold">{paket.namaPaket}</h1>
-                            <p className="text-sm">{`Rp.${Math.min(...paket.menu.map(menu => menu.price))}-Rp.${Math.max(...paket.menu.map(menu => menu.price))}`}</p>
-                            <p className="text-xs">{`${paket.menu.length} menu`}</p>
-                            <div className="flex justify-between mt-[10px]">
-                                <button className=" w-16 bg-red-600 hover:bg-red-900 text-white transition-colors h-7 rounded-xl">Hapus</button>
+                        <div className="flex flex-col ">
+                            <div>
+                                <h1 className="text-accent-200 font-semibold">{paket.namaPaket}</h1>
+                                <p className="text-sm font-medium">{`Rp.${Math.min(...paket.menu.map(menu => menu.price))}-Rp.${Math.max(...paket.menu.map(menu => menu.price))}`}</p>
+                                <p className="text-xs">{`${paket.menu.length} menu`}</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <button className=" w-16  bg-red-900 text-white  transition-colors h-7 rounded-xl">Hapus</button>
                                 <button className="bg-accent-200 text-white hover:bg-[#00454f]  w-16    h-7  rounded-xl  ">Edit</button>
                             </div>
                         </div>
                     </div>
-
                 ))}
-            </div>s
+            </div>
         </div>
     )
 }
--- frontend/src/seller/pages/products.jsx
+++ frontend/src/seller/pages/products.jsx
@@ -12,7 +12,7 @@ function Products() {
 
 
     return (
-        <div className={`flex items-start h-auto  transition-all ease-in-out duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-1'}`}>
+        <div className={`flex items-start bg-bg-300 h-screen overflow-auto transition-all ease-in-out duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-1'}`}>
             {isOpen && <Sidebar />}
             <button className='z-20 hover:bg-bg-300 rounded-full transition-colors ' onClick={toggleSidebar}>
                 <Hamburger size={20} easing="ease-in" color='black' toggled={isOpen} toggle={setIsOpen} />
