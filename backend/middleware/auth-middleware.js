0055712783595613cb83d88d1b62857d81059cce
Galang Arsandy <galangarsandy@gmail.com>
2023-12-14 02:29:02 +0700

    Menambahkan middleware Authentication & Authorization

--- backend/middleware/auth-middleware.js
+++ backend/middleware/auth-middleware.js
@@ -3,35 +3,45 @@ require('dotenv').config();
 
 const SECRET = process.env.SECRET_KEY
 
-function verifyToken(req, res, next) {
+function auth(req, res, next) {
     const auth = req.headers.authorization
-
-    if (!auth) {
-        res.status(403).json({
-            message: "unauthorized-user"
-        })
-        return
-    } else {
-        const token = auth.split(" ")[1]
-        if (!token) {
-            res.json({
-                message: "undifined-token"
+    try {
+        if (!auth) {
+            res.status(403).json({
+                code: "UN-AUTH",
+                message: "unauthorized-user"
             })
             return
         } else {
-            try {
-                var decoded = jwt.verify(token, SECRET);
-                req.payload = decoded
-            } catch (err) {
-                res.json({
-                    status: "invalid",
-                    message: err.message
+            const token = auth.split(" ")[1]
+            if (!token) {
+                res.status(403).json({
+                    code: "UN-AUTH",
+                    message: "unauthorized-user"
                 })
                 return
+            } else {
+                try {
+                    var decoded = jwt.verify(token, SECRET);
+                    req.payload = decoded
+                } catch (err) {
+                    res.status(406).json({
+                        status: "TOKEN-ERR",
+                        message: err.message
+                    })
+                    return
+                }
             }
         }
+    } catch (err) {
+        res.status(500).json({
+            status: "ERR",
+            message: err.message
+        })
+        return
     }
+
 
     next()
 }
-module.exports = verifyToken
+module.exports = auth
